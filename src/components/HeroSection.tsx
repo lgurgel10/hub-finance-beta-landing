90346e681305ff0ef49c11d76078ca3c75f45627
lucas <gurgellucas461@gmail.com>
2025-05-02 19:07:34 -0300

    ajustando padding novamente para o mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="relative min-h-screen flex items-center justify-center bg-dark-700">
+    <div className="relative min-h-[100dvh] flex items-center justify-center bg-dark-700 py-16">
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto text-center">
           <h1 className="text-4xl lg:text-6xl font-extrabold mb-6">
